5b3e4f595465a70cbf89ada3cd8282552d48aa2c
David Portilla <david_portilla@hotmail.com>
2021-02-02 10:51:21 -0500

    adding useCriptoCurrency hook

---
+++ src/hooks/useCriptoCurrency.js
@@ -0,0 +1,55 @@
+import React, {Fragment, useState} from 'react'
+import styled from '@emotion/styled'
+
+const Label = styled.label`
+  font-family: 'Bebas Neue', cursive;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: 900;
+  font-size: 2.4rem;
+  margin-top: 2rem;
+  display: block;
+`
+
+const SelectTag = styled.select`
+  width: 100%;
+  display: block;
+  padding: 1rem;
+  -webkit-appearance: none;
+  border-radius: 10px;
+  border: none;
+  font-size: 1.2rem;
+`
+
+const useCriptoCurrency = (label, initialState, options) => {
+
+  console.log(options)
+
+  // Custom hook state
+  const [state, updateState] = useState(initialState)
+
+  const SelectCripto = () => (
+    <Fragment>
+      <Label>{label}</Label>
+      <SelectTag
+        onChange={e => updateState(e.target.value)}
+        value={state}
+      >
+        <option value="">-- Select a cripto --</option>
+        {/* {options.map(option => (
+          <option
+            key={option.cod}
+            value={option.cod}
+          >
+            {option.name}
+          </option>
+        ))
+        } */}
+      </SelectTag>
+    </Fragment>
+  )
+
+  return [state, SelectCripto, updateState]
+}
+
+export default useCriptoCurrency
